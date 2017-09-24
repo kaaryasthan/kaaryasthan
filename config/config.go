2149bb2e8d214309c497210ca45b5a65067f2444
Baiju Muthukadan <baiju.m.mail@gmail.com>
2017-09-25 00:19:24 +0530

    include database name

--- config/config.go
+++ config/config.go
@@ -12,6 +12,7 @@ type configuration struct {
 	PostgresHost       string   `default:"localhost" split_words:"true"`
 	PostgresPort       int      `default:"5433" split_words:"true"`
 	PostgresUser       string   `default:"postgres" split_words:"true"`
+	PostgresDatabase   string   `default:"postgres" split_words:"true"`
 	PostgresPassword   string   `default:"secret" split_words:"true"`
 	PostgresSSLMode    string   `default:"disable" envconfig:"POSTGRES_SSL_MODE"`
 	HTTPAddress        string   `default:":8080" envconfig:"HTTP_ADDRESS"`
@@ -24,10 +25,11 @@ type configuration struct {
 }
 
 func (c *configuration) PostgresConfig() string {
-	return fmt.Sprintf("host=%s port=%d user=%s password=%s sslmode=%s",
+	return fmt.Sprintf("host=%s port=%d user=%s dbname=%s password=%s sslmode=%s",
 		c.PostgresHost,
 		c.PostgresPort,
 		c.PostgresUser,
+		c.PostgresDatabase,
 		c.PostgresPassword,
 		c.PostgresSSLMode,
 	)
