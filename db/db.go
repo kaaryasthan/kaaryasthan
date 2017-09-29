49de0fe3f1bb9a4115933df18ee912063f4ccdef
Baiju Muthukadan <baiju.m.mail@gmail.com>
2017-09-30 00:14:13 +0530

    retry DB connection

--- db/db.go
+++ db/db.go
@@ -3,8 +3,10 @@ package db
 import (
 	"database/sql"
 	"log"
+	"time"
 
 	"github.com/baijum/pgmigration"
+	"github.com/jpillora/backoff"
 	"github.com/kaaryasthan/kaaryasthan/config"
 	// DB is actually initialized here
 	_ "github.com/lib/pq"
@@ -17,8 +19,30 @@ func init() {
 	var err error
 	DB, err = sql.Open("postgres", config.Config.PostgresConfig())
 	if err != nil {
-		log.Fatal(err.Error())
+		log.Fatal(err)
 	}
+
+	_, err = DB.Exec("SELECT 1") // DB.Ping() seems to be not working always
+
+	b := &backoff.Backoff{
+		Min:    7 * time.Second,
+		Factor: 2,
+		Max:    7 * time.Minute,
+	}
+
+	go func() {
+		for {
+			_, err = DB.Exec("SELECT 1") // DB.Ping() seems to be not working always
+			if err != nil {
+				d := b.Duration()
+				log.Printf("%s (pinging failed), reconnecting in %s", err, d)
+				time.Sleep(d)
+				continue
+			}
+			b.Reset()
+			time.Sleep(b.Max)
+		}
+	}()
 }
 
 // SchemaMigrate migrate database schema
