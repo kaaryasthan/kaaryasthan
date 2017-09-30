f6bc3b02a026e98db93deed363605a04752fc514
Baiju Muthukadan <baiju.m.mail@gmail.com>
2017-09-30 09:58:08 +0530

    arrange order of columns

--- db/migrations/000000-base.sql
+++ db/migrations/000000-base.sql
@@ -1,42 +1,42 @@
 CREATE TABLE "projects" (
-  created_at TIMESTAMP WITH TIME ZONE,
-  updated_at TIMESTAMP WITH TIME ZONE, 
-  deleted_at TIMESTAMP WITH TIME ZONE,
   id BIGSERIAL PRIMARY KEY,
   name TEXT NOT NULL,
-  description TEXT NOT NULL
+  description TEXT NOT NULL,
+  created_at TIMESTAMP WITH TIME ZONE,
+  updated_at TIMESTAMP WITH TIME ZONE,
+  deleted_at TIMESTAMP WITH TIME ZONE
 );
 
 CREATE UNIQUE INDEX projects_name_idx ON projects (name) WHERE deleted_at IS NULL;
 
 CREATE TABLE "items" (
-  created_at TIMESTAMP WITH TIME ZONE,
-  updated_at TIMESTAMP WITH TIME ZONE, 
-  deleted_at TIMESTAMP WITH TIME ZONE,
   id BIGSERIAL PRIMARY KEY,
   title TEXT NOT NULL,
-  description TEXT NOT NULL
+  description TEXT NOT NULL,
+  created_at TIMESTAMP WITH TIME ZONE,
+  updated_at TIMESTAMP WITH TIME ZONE,
+  deleted_at TIMESTAMP WITH TIME ZONE
 );
 
 CREATE TABLE "comments" (
-  created_at TIMESTAMP WITH TIME ZONE,
-  updated_at TIMESTAMP WITH TIME ZONE, 
-  deleted_at TIMESTAMP WITH TIME ZONE,
   id BIGSERIAL PRIMARY KEY,
   body TEXT NOT NULL,
-  item BIGINT REFERENCES items(id)
+  item BIGINT REFERENCES items(id),
+  created_at TIMESTAMP WITH TIME ZONE,
+  updated_at TIMESTAMP WITH TIME ZONE,
+  deleted_at TIMESTAMP WITH TIME ZONE
 );
 
 CREATE TABLE "members" (
-  created_at TIMESTAMP WITH TIME ZONE,
-  updated_at TIMESTAMP WITH TIME ZONE, 
-  deleted_at TIMESTAMP WITH TIME ZONE,
   id BIGSERIAL PRIMARY KEY,
   username TEXT NOT NULL,
   name TEXT NOT NULL,
   email TEXT NOT NULL,
   password BYTEA NOT NULL,
-  salt BYTEA NOT NULL
+  salt BYTEA NOT NULL,
+  created_at TIMESTAMP WITH TIME ZONE,
+  updated_at TIMESTAMP WITH TIME ZONE,
+  deleted_at TIMESTAMP WITH TIME ZONE
 );
 
 CREATE UNIQUE INDEX members_username_idx ON members (username) WHERE deleted_at IS NULL;
