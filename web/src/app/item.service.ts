f263d24cb0c2aa7eb56722870e9badce02aecf5f
Baiju Muthukadan <baiju.m.mail@gmail.com>
2018-04-18 08:13:00 +0530

    cp

--- web/src/app/item.service.ts
+++ web/src/app/item.service.ts
@@ -6,6 +6,13 @@ import 'rxjs/add/operator/map';
 
 import { Item } from './item';
 
+export class ItemModel {
+    id = 0;
+    title = "";
+    description = "";
+    project_id = 0;
+}
+
 @Injectable()
 export class ItemService {
     private itmUrl = 'api/v1/items';
@@ -36,6 +43,34 @@ export class ItemService {
         return this.http
             .post(this.itmUrl, entity, httpOptions)
             .map(data => data['data'].attributes);
+    }
+
+    search(text: string): Observable<Item[]> {
+        const httpOptions = {
+            headers: new HttpHeaders({
+                'Content-Type': 'application/vnd.api+json',
+                'Authorization': 'Bearer ' + localStorage.getItem('currentUser'),
+            })
+        };
+
+        console.log('Bearer ' + localStorage.getItem('currentUser'));
+
+        return this.http
+            .get(this.itmUrl + "?Query=" + text, httpOptions)
+            .map(data => {
+                var itmList: ItemModel[] = [];
+                for (let i = 0; i < data['data'].length; i++) {
+                    var o = data['data'][i];
+                    var itm = new ItemModel();
+                    itm.id = o.id;
+                    itm.title = o.attributes.title;
+                    itm.description = o.attributes.description;
+                    itm.project_id = o.attributes.project_id;
+                    itmList.push(itm);
+                }
+                return itmList;
+            });
+
     }
 
 }
